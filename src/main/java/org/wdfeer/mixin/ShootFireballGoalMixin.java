84da393d92eea79be90d12b80183179e62450889
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-07-12 23:11:41 +0200

    Make blaze a machine gun

--- src/main/java/org/wdfeer/mixin/ShootFireballGoalMixin.java
+++ src/main/java/org/wdfeer/mixin/ShootFireballGoalMixin.java
@@ -8,11 +8,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(targets = "net/minecraft/entity/mob/BlazeEntity$ShootFireballGoal")
 public class ShootFireballGoalMixin {
-    @Shadow private int fireballCooldown;
+    @Shadow private int fireballsFired;
 
     @Inject(method = "tick", at = @At("RETURN"))
     private void injectTick(CallbackInfo ci) {
-        if (this.fireballCooldown > 6)
-            this.fireballCooldown = 6;
+        fireballsFired = fireballsFired % 3 + 1;
     }
 }
